a7936d4b8603440e9263f5a26da875f37c74a547
Heloísa Saldanha <heloisa.saldanha@gmail.com>
2022-02-11 13:12:02 -0300

    concluded

---
+++ accountbank/src/main/kotlin/com/example/accountbank/AccountController.kt
@@ -0,0 +1,34 @@
+package com.example.accountbank
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("accounts")
+class AccountController(
+    val repository: AccountRepository
+){
+    @PostMapping
+    fun create(@RequestBody account: Account) = ResponseEntity.ok(repository.save(account))
+
+    @GetMapping("accounts/list")
+    fun read(): ResponseEntity<MutableList<Account>> {
+        return ResponseEntity.ok(repository.findAll())
+    }
+
+    @PutMapping("{document}")
+    fun update(@PathVariable document : String, @RequestBody account: Account): ResponseEntity<Account>{
+        val accountDBOptional = repository.findByDocument(document)
+        val toSave = accountDBOptional.orElseThrow{RuntimeException("Account document $document not found!")}.copy(
+            name = account.name, balance = account.balance
+        )
+        return ResponseEntity.ok(repository.save(toSave))
+    }
+
+    @DeleteMapping("{document}")
+    fun delete(@PathVariable document : String) = repository.findByDocument(document).ifPresent {
+        repository.delete(it)
+    }
+}
+
+
